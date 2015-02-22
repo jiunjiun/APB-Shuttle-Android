686c17220c6ef88686d9db5dad672fcadf75cb9f
jiunjiun <quietmes@gmail.com>
2015-02-23 04:24:18 +0800

    完成網頁版的首頁 - 除了 touch 手勢

--- app/src/main/java/tw/jiunjiun/apb/shuttle/IndexFragment.java
+++ app/src/main/java/tw/jiunjiun/apb/shuttle/IndexFragment.java
@@ -8,12 +8,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import tw.jiunjiun.apb.shuttle.curl.APB_API;
 import tw.jiunjiun.apb.shuttle.parser.ParserBus;
 
 import static tw.jiunjiun.apb.shuttle.curl.APB_API.KIND_NOW;
+import static tw.jiunjiun.apb.shuttle.curl.APB_API.KIND_NEXT;
 
 /**
  * Created by jiun on 2015/2/22.
@@ -22,7 +25,11 @@ public class IndexFragment extends Fragment {
     private static final String TAG = "IndexFragment";
     private ParserBus mParserBus;
 
+    private int NextNum = 0;
+
     private TextView depart, details;
+    private ImageButton logo;
+    private Button paginate_previous, paginate_next;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -59,6 +66,40 @@ public class IndexFragment extends Fragment {
     private View initView(View mView) {
         depart  = (TextView) mView.findViewById(R.id.depart);
         details = (TextView) mView.findViewById(R.id.details);
+
+        logo   = (ImageButton) mView.findViewById(R.id.logo);
+        paginate_previous = (Button) mView.findViewById(R.id.paginate_previous);
+        paginate_next     = (Button) mView.findViewById(R.id.paginate_next);
+
+
+        logo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new APB_API(mHandler, KIND_NOW);
+                NextNum = 0;
+                paginate_previous.setVisibility(View.GONE);
+            }
+        });
+
+        paginate_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NextNum--;
+                if(NextNum == 0) paginate_previous.setVisibility(View.GONE);
+                new APB_API(mHandler, KIND_NEXT, new int[]{NextNum});
+            }
+        });
+
+        paginate_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NextNum++;
+                if(NextNum == 1) paginate_previous.setVisibility(View.VISIBLE);
+                new APB_API(mHandler, KIND_NEXT, new int[]{NextNum});
+            }
+        });
+
+
         return mView;
     }
 
@@ -71,6 +112,7 @@ public class IndexFragment extends Fragment {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case KIND_NOW:
+                case KIND_NEXT:
                     mParserBus = new ParserBus(msg.obj.toString());
                     setupView();
                     break;
